ecb1ca095969a5367f06f624fad3931510e3b88d
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2020-10-18 03:03:13 +0500

    add NavLink and get user details
    Add navlink and get the user event when login and logout using Netlify Identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,24 +1,50 @@
-import React, {useEffect} from "react";
-import { Flex, Container, Heading, Button } from "theme-ui";
+import React, { useEffect, useState } from "react";
+import { Flex, Container, Heading, Button, NavLink } from "theme-ui";
 const NetlifyIdentityWidget = require("netlify-identity-widget");
+import {Link} from 'gatsby';
 
-export default props => {
+export default (props) => {
 
-  useEffect( () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
     NetlifyIdentityWidget.init({});
   });
 
+  NetlifyIdentityWidget.on("login", user => {
+    NetlifyIdentityWidget.close();
+    setUser(user);
+  });
+
+  NetlifyIdentityWidget.on("logout", () => {
+    NetlifyIdentityWidget.close();
+    setUser();
+  })
+
   return (
     <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>        
+        { user && (<NavLink p={2}>
+          {user.user_metadata.full_name}
+        </NavLink>) }
+      </Flex>
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
         <Heading as="h1">Todo App</Heading>
-        <Button sx={{marginTop: 2}} 
-            onClick={() => {NetlifyIdentityWidget.open()}}
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            NetlifyIdentityWidget.open();
+          }}
         >
-            Login
+          Login
         </Button>
       </Flex>
     </Container>
   );
 };
-
