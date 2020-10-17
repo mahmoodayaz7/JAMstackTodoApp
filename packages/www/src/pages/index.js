9fce3778b1d54b0581cd8fd11defad396d4d0cf1
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2020-10-18 01:28:02 +0500

    Added Theme-UI
    Added Theme-UI

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Flex, Container, Heading, Button } from "theme-ui";
 export const index = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Todo App</Heading>
+        <Button sx={{marginTop: 2}} 
+            onClick={() => alert('Login Button Click')}
+        >
+            Login
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
 
 export default index;
---
+++ packages/www/wrap-root-element.js
@@ -0,0 +1,14 @@
+// basic usage
+const React = require('react')
+const { ThemeProvider } = require('theme-ui')
+const {deep} = require('@theme-ui/presets')
+
+const newTheme = {
+  ...deep,
+  sizes: {container: 1024}
+};
+
+module.exports = ({element}) => (
+  <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
+);
+ 
